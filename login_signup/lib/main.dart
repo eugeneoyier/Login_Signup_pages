2150dfa27ec8f27588e80f1425186df6b66e99fe
eugeneoyier <158266046+eugeneoyier@users.noreply.github.com>
2024-02-14 07:24:35 +0300

    final

--- login_signup/lib/main.dart
+++ login_signup/lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:login_signup/screens/welcome_screen.dart';
+import 'package:login_signup/theme/theme.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //This widge is te root of your application
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        ),
-        home: const WelcomeScreen(),
+      theme: lightMode,
+      home: const WelcomeScreen(),
     );
   }
 }
